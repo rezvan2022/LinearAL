2ab0094f1d3c926860cff80ae200366679a773f0
Rezvan Abdollahi <115983981+rezvan2022@users.noreply.github.com>
2022-10-26 10:14:11 +0330

    Update Divdif_main.m

--- LinearAL/LinearALG 3/Divdif_main.m
+++ LinearAL/LinearALG 3/Divdif_main.m
@@ -1,6 +1,9 @@
 X=[0,0.5,1,1.5,2];
 Y=sin(X);
-[C,D] = Newton_divdiff(X,Y);
+[C,D] = Newton_divdiff(X,Y)
+% Given a sequence of data points(x0,y0),... ,(xn,yn)}
+%the method calculates the coefficients of the interpolation polynomial of these points in the Newton form.
+%Newton_divdiff(X,Y) is a code that deployed to model this method
 A=hilb(2);
 A_f=polyvalm(C,A)
 F = funm(A,@sin)
